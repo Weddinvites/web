c3a8293ccdc1cd02ae886b5b70f9b20517833795
HectorCavilla <hector.cavilla@gmail.com>
2024-05-01 20:08:52 -0500

    Add: Vercel deployment config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import node from "@astrojs/node";
+//import node from "@astrojs/node";
 //import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
@@ -9,9 +9,6 @@ import node from "@astrojs/node";
   output: 'hybrid',
   integrations: [tailwind(), react()],
   adapter: vercel() */
-
-
-import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
@@ -22,9 +19,5 @@ export default defineConfig({
     routing: {
       prefixDefaultLocale: false
     }
-  },
-  output: "server",
-  adapter: node({
-    mode: "standalone"
-  })
+  }
 });
