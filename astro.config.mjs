48fa1586640953aacab4ea9ba14d0c6c09188dc7
HectorCavilla <hector.cavilla@gmail.com>
2024-05-08 19:09:32 -0500

    Try: fixing SSR functionalities

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
@@ -13,5 +14,5 @@ export default defineConfig({
     }
   },
   output: 'hybrid',
-
+  adapter: vercel()
 })
