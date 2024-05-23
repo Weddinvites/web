8fdfba30e6d5148b92384e7ccd82407a0a2c76a1
HectorCavilla <hector.cavilla@gmail.com>
2024-05-23 13:48:05 -0500

    Add: Analytics and sitemap

--- astro.config.js
+++ astro.config.js
@@ -2,10 +2,30 @@ import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import sitemap from '@astrojs/sitemap';
+import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  site: 'https://weddinvites.com',
+  integrations: [
+    tailwind(),
+    react(),
+    partytown({
+			config: {
+			  forward: ["dataLayer.push"],
+			},
+		}),
+    sitemap({
+      i18n: {
+        defaultLocale: 'es',
+        locales: {
+          es: 'es',
+          en: 'en'
+        },
+      },
+    })
+  ],
   i18n: {
     defaultLocale: "es",
     locales: ["es", "en"],
